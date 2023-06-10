9c2eb0d07cd8a1a84f5cf26613bcb7d2e96b13f5
shinshin18 <shinshin_18@hotmail.com>
2023-06-10 11:20:49 +0900

    OrientationObserver

---
+++ swiftui02/screen34.swift
@@ -0,0 +1,20 @@
+//
+//  screen34.swift
+//  swiftui02
+//
+//  Created by Nobuhito Ogawa on 2023/06/10.
+//
+
+import SwiftUI
+
+struct screen34: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct screen34_Previews: PreviewProvider {
+    static var previews: some View {
+        screen34()
+    }
+}
